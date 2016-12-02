e869897b1376a42f9f4d7b5b68453fcb77eddbb2
Matthew Wilkinson <Wilkinson.M@outlook.com>
2016-12-02 09:05:02 -0500

    Add project files.

---
+++ WipeIt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WipeIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
