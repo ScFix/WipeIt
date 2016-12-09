e3e27f6d080e81d30f61a0e05d2e40dca0e56d15
Matthew Wilkinson <Wilkinson.M@outlook.com>
2016-12-09 01:05:49 -0500

    Put it down to days

--- WipeIt/Program.cs
+++ WipeIt/Program.cs
@@ -10,48 +10,55 @@ using RedditSharp.Things;
 
 namespace WipeIt
 {
-    class Program
-    {
-        static DateTime threshold = DateTime.Now.AddMonths(-2);
+	class Program
+	{
 
-        static void Main(string[] args)
-        {
-            var userName = args[0];
-            var password = args[1];
+		static void Main(string[] args)
+		{
+			var userName = args[0];
+			var password = args[1];
+			int days = 0;
 
-            var reddit = new Reddit(userName, password, true);
-            Console.WriteLine("Processing");
-            int processed, internalSkip, globalSkip = 0;
-            do
-            {
-                processed = 0;
-                internalSkip = 0;
-                foreach (var comment in reddit.User.Comments.Skip(globalSkip).Take(25))
-                {
+			int.TryParse(args[2], out days);
+			if (days != 0)
+			{
+				days = days * -1;
+				DateTime threshold = DateTime.Now.AddDays(days);
 
-                    if (IsCommentOld(comment))
-                    {
-                        Console.WriteLine(comment.Created);
-                        comment.Del();
-                        processed++;
-                    }
-                    else
-                    {
-                        internalSkip++;
+				var reddit = new Reddit(userName, password, true);
+				Console.WriteLine("Processing");
+				int processed, internalSkip, globalSkip = 0;
+				do
+				{
+					processed = 0;
+					internalSkip = 0;
+					foreach (var comment in reddit.User.Comments.Skip(globalSkip).Take(25))
+					{
 
-                    }
-                }
-                Console.WriteLine("Processed a total of " + processed);
-                globalSkip += internalSkip;
-            } while (processed > 0 || internalSkip == 25);
+						if (comment.Created < threshold)
+						{
+							Console.WriteLine(comment.Created);
+							comment.Del();
+							processed++;
+						}
+						else
+						{
+							internalSkip++;
 
-            Console.WriteLine("Done Processing");
-            Console.ReadKey();
-        }
+						}
+					}
+					Console.WriteLine("Processed a total of " + processed);
+					globalSkip += internalSkip;
+				} while (processed > 0 || internalSkip == 25);
 
-        private static bool IsCommentOld(Comment comment)
-        {
-            return comment.Created < threshold;
-        }
-    }
+				Console.WriteLine("Done Processing");
+				Console.ReadKey();
+			}
+			else
+			{
+				Console.WriteLine("Not able to process due to missing days figure");
+			}
+		}
+
+	}
 }
